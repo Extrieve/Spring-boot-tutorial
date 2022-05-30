14a1d6e57a6fe456a4b76c0202b1af88afb597ee
Nick <nicolas.cinera@gmail.com>
2022-05-30 11:42:54 -0400

    Lombok
    - Similar to data classes in Python?
    - Will have to look into performance comparisons

--- src/main/java/com/extrieve/Springboot/tutorial/entity/Department.java
+++ src/main/java/com/extrieve/Springboot/tutorial/entity/Department.java
@@ -1,5 +1,10 @@
 package com.extrieve.Springboot.tutorial.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +12,10 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
 
     @Id
@@ -17,58 +26,5 @@ public class Department {
     private String departmentAddress;
     private String departmentCode;
 
-
-    public Department() {
-    }
-
-    public Department(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
 
 }
